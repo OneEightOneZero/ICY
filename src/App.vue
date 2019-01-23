1e8ab7ff5a0197cc3ae03df255481dad32c74ba3
raymichan <raymichan@163.com>
2019-01-23 15:35:00 +0800

    text2

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,29 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <Xheader/>
-<<<<<<< HEAD
     <Cfooter></Cfooter>
-=======
     <Xiconnav/>
     
->>>>>>> 799b4160bd601f15c9cb569b2542b713decd509f
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-<<<<<<< HEAD
 import Xheader from './components/Xheader.vue'
 import Cfooter from './components/Cfooter'
-=======
 
-import Xheader from "./components/Xheader.vue";
 import Xiconnav from "./components/Xiconnav.vue";
 
->>>>>>> 799b4160bd601f15c9cb569b2542b713decd509f
 
 export default {
   name: "app",
   components: {
-    // HelloWorld
-<<<<<<< HEAD
-    Xheader,
-    Cfooter
-=======
+   
     // Xheaderstyle,
     Xheader,
+    Cfooter,
     Xiconnav,
     // Xheadershop
->>>>>>> 799b4160bd601f15c9cb569b2542b713decd509f
   }
 };
 </script>
